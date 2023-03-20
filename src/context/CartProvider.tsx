3ca0f28a81410a6cb2e62ee7e14543ac0b440700
yanliu1111 <liuyan0504@gmail.com>
2023-03-19 19:20:20 -0600

    context done w useReducer userMemo

---
+++ src/context/CartProvider.tsx
@@ -0,0 +1,111 @@
+import { useMemo, useReducer } from "react";
+
+export type CartItemType = {
+  sku: string;
+  name: string;
+  price: number;
+  qty: number;
+};
+type CartStateType = { cart: CartItemType[] };
+const initCartState: CartStateType = { cart: [] };
+
+const REDUCER_ACTION_TYPE = {
+  ADD: "ADD",
+  REMOVE: "REMOVE",
+  QUANTITY: "QUANTITY",
+  SUBMIT: "SUBMIT",
+};
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE;
+
+export type ReducerAction = {
+  type: string;
+  payload?: CartItemType;
+};
+const reducer = (
+  state: CartStateType,
+  action: ReducerAction
+): CartStateType => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.ADD: {
+      if (!action.payload) {
+        throw new Error("action.payload missing in ADD action");
+      }
+      const { sku, name, price } = action.payload;
+
+      const filteredCart: CartItemType[] = state.cart.filter(
+        (item) => item.sku !== sku
+      );
+
+      const itemExists: CartItemType | undefined = state.cart.find(
+        (item) => item.sku === sku
+      );
+
+      const qty: number = itemExists ? itemExists.qty + 1 : 1;
+
+      return { ...state, cart: [...filteredCart, { sku, name, price, qty }] };
+    }
+
+    case REDUCER_ACTION_TYPE.REMOVE: {
+      if (!action.payload) {
+        throw new Error("action.payload missing in REMOVE action");
+      }
+      const { sku } = action.payload;
+
+      const filteredCart: CartItemType[] = state.cart.filter(
+        (item) => item.sku !== sku
+      );
+      return { ...state, cart: [...filteredCart] };
+    }
+    case REDUCER_ACTION_TYPE.QUANTITY: {
+      if (!action.payload) {
+        throw new Error("action.payload missing in QUANTITY action");
+      }
+      const { sku, qty } = action.payload;
+
+      const itemExists: CartItemType | undefined = state.cart.find(
+        (item) => item.sku === sku
+      );
+      if (!itemExists) {
+        throw new Error("Item must exist in order to update quantity");
+      }
+      const updateItem: CartItemType = { ...itemExists, qty };
+      const filteredCart: CartItemType[] = state.cart.filter(
+        (item) => item.sku !== sku
+      );
+      return { ...state, cart: [...filteredCart, updateItem] }; //...filteredCart means we are spreading the array and then adding the updateItem to the end of the array, this is the same as filteredCart.concat(updateItem)
+      //spread operator is the same as concat
+    }
+    case REDUCER_ACTION_TYPE.SUBMIT: {
+      return { ...state, cart: [] };
+    }
+    default:
+      throw new Error("Unidentified reducer action type");
+  }
+};
+const useCartContext = (initCartState: CartStateType) => {
+  const [state, dispatch] = useReducer(reducer, initCartState);
+  //what we done is memoize that reducer action type so its always so it always has the same referential equality when we pass it into a component and that will help us memoize the component in the future without worrying about the reducer actions causing a re-render
+  const REDUCER_ACTIONS = useMemo(() => {
+    return REDUCER_ACTION_TYPE;
+  }, []);
+  const totalItems: number = state.cart.reduce((previousValue, cartItem) => {
+    return previousValue + cartItem.qty;
+  }, 0); // reducer needs the initial value, in this case it's 0
+  const totalPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(
+    state.cart.reduce((previousValue, cartItem) => {
+      return previousValue + cartItem.qty * cartItem.price;
+    }, 0)
+  );
+  // set cart to sort by sku
+  const cart = state.cart.sort((a, b) => {
+    const itemA = Number(a.sku.slice(-4));
+    const itemB = Number(b.sku.slice(-4));
+    return itemA - itemB;
+  });
+
+  // I created custom hook that doesn't mean they won't cause a re-render, it is just another way to approach it and memorization only helps when we would use react.memo, that would only help if we were passing things down as props
+  return { dispatch, REDUCER_ACTIONS, cart, totalItems, totalPrice };
+};
---
+++ src/context/ProductsProvider.tsx
@@ -0,0 +1,57 @@
+import { createContext, ReactElement, useEffect, useState } from "react";
+
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+// we also can do the json server in a Dev environment and then use the fetch api to get the data from the server
+const initState: ProductType[] = [];
+
+// const initState: ProductType[] = [
+//   {
+//     sku: "item0001",
+//     name: "Widget",
+//     price: 9.99,
+//   },
+//   {
+//     sku: "item0002",
+//     name: "Premium Widget",
+//     price: 19.99,
+//   },
+//   {
+//     sku: "item0003",
+//     name: "Deluxe Widget",
+//     price: 29.99,
+//   },
+// ];
+export type UseProductsContextType = { products: ProductType[] };
+
+const initContextState: UseProductsContextType = { products: [] };
+const ProductsContext = createContext<UseProductsContextType>(initContextState);
+//from the context lesson, we need to provide the children type as we create provider in react 18 and above. It used to be ok to have that implicit in old version.
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
+  const [products, setProducts] = useState<ProductType[]>(initState);
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch("http://localhost:3500/products")
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+    fetchProducts().then((products) => setProducts(products));
+  }, []);
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+//we wrap around the children, notice they are not passed in like we would a prop like the products for example it's acctually what's between the opening and closing tags of the component
+export default ProductsContext;
