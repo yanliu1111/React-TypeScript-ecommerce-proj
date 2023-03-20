854de63447be1658a1c31cbe78540266be44d080
yanliu1111 <liuyan0504@gmail.com>
2023-03-20 10:56:40 -0600

    Feat: header and footer done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import useCart from "../hooks/useCart";
 import Nav from "./Nav";
 
 type PropsType = {
@@ -5,19 +6,21 @@ type PropsType = {
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
 const Header = ({ viewCart, setViewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart();
+
   const content = (
     <header className="header">
       <div className="header__title-bar">
         <h1>Acme Co.</h1>
         <div className="header__price-box">
-          <p>Total Items:</p>
-          <p>Total Price:</p>
+          <p>Total Items: {totalItems}</p>
+          <p>Total Price: {totalPrice}</p>
         </div>
       </div>
       <Nav viewCart={viewCart} setViewCart={setViewCart} />
     </header>
   );
-  return <div>Header</div>;
-};
 
+  return content;
+};
 export default Header;
